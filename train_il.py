4b16950c570bfc89fa0404ed468153b7b2bf3dfe
Chenye Zhu <zhuchenye@gmail.com>
2022-03-06 11:30:51 -0800

    Problem 2

--- train_il.py
+++ train_il.py
@@ -5,10 +5,11 @@ from utils import *
 
 tf.config.run_functions_eagerly(True)
 
+
 class NN(tf.keras.Model):
     def __init__(self, in_size, out_size):
         super(NN, self).__init__()
-        
+
         ######### Your code starts here #########
         # We want to define and initialize the weights & biases of the neural network.
         # - in_size is dim(O)
@@ -17,9 +18,14 @@ class NN(tf.keras.Model):
         #         - tf.keras.initializers.GlorotUniform (this is what we tried)
         #         - tf.keras.initializers.GlorotNormal
         #         - tf.keras.initializers.he_uniform or tf.keras.initializers.he_normal
-
-
-
+        self.dense1 = tf.keras.layers.Dense(9, activation="relu", kernel_initializer="he_normal")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.dropout1 = tf.keras.layers.Dropout(0.1)
+        self.dense2 = tf.keras.layers.Dense(5, activation="relu", kernel_initializer="he_normal")
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.dropout2 = tf.keras.layers.Dropout(0.1)
+        self.dense3 = tf.keras.layers.Dense(out_size, kernel_initializer="he_normal")
+        self.bn3 = tf.keras.layers.BatchNormalization()
         ########## Your code ends here ##########
 
     def call(self, x):
@@ -27,13 +33,15 @@ class NN(tf.keras.Model):
         ######### Your code starts here #########
         # We want to perform a forward-pass of the network. Using the weights and biases, this function should give the network output for x where:
         # x is a (?,|O|) tensor that keeps a batch of observations
+        out1 = self.dropout1(self.bn1(self.dense1(x)))
+        out2 = self.dropout2(self.bn2(self.dense2(out1)))
+        out3 = self.dense3(out2)
 
-
-
+        return out3
         ########## Your code ends here ##########
 
 
-def loss(y_est, y):
+def loss(y_est, y, steer_dimension=0, throttle_dimension=1):
     y = tf.cast(y, dtype=tf.float32)
     ######### Your code starts here #########
     # We want to compute the loss between y_est and y where
@@ -41,11 +49,16 @@ def loss(y_est, y):
     # - y is the actions the expert took for the corresponding batch of observations
     # At the end your code should return the scalar loss value.
     # HINT: Remember, you can penalize steering (0th dimension) and throttle (1st dimension) unequally
+    steering_weight = 1e3
+    throttle_weight = 1
+    action_losses = tf.reduce_mean(tf.square(y_est - y), axis=0)
 
+    steering_loss = action_losses[steer_dimension]
+    throttle_loss = action_losses[throttle_dimension]
 
-
+    return steering_weight * steering_loss + throttle_weight * throttle_loss
     ########## Your code ends here ##########
-    
+
 
 def nn(data, args):
     """
@@ -56,7 +69,7 @@ def nn(data, args):
     }
     in_size = data['x_train'].shape[-1]
     out_size = data['y_train'].shape[-1]
-    
+
     nn_model = NN(in_size, out_size)
     if args.restore:
         nn_model.load_weights('./policies/' + args.scenario.lower() + '_' + args.goal.lower() + '_IL')
@@ -73,8 +86,6 @@ def nn(data, args):
         # 3. Based on the loss calculate the gradient for all weights
         # 4. Run an optimization step on the weights.
         # Helpful Functions: tf.GradientTape(), tf.GradientTape.gradient(), tf.keras.Optimizer.apply_gradients
-        
-        
 
         ########## Your code ends here ##########
 
@@ -85,7 +96,8 @@ def nn(data, args):
         for x, y in train_data:
             train_step(x, y)
 
-    train_data = tf.data.Dataset.from_tensor_slices((data['x_train'], data['y_train'])).shuffle(100000).batch(params['train_batch_size'])
+    train_data = tf.data.Dataset.from_tensor_slices((data['x_train'], data['y_train'])).shuffle(100000).batch(
+        params['train_batch_size'])
 
     for epoch in range(args.epochs):
         # Reset the metrics at the start of the next epoch
@@ -106,9 +118,9 @@ if __name__ == '__main__':
     parser.add_argument("--lr", type=float, help="learning rate for Adam optimizer", default=5e-3)
     parser.add_argument("--restore", action="store_true", default=False)
     args = parser.parse_args()
-    
+
     maybe_makedirs("./policies")
-    
+
     data = load_data(args)
 
     nn(data, args)
